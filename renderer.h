144192194c1a445249be2f1d8ab74a87ef2dfe6d
abcight <abcightdev@gmail.com>
2024-06-16 15:30:09 +0200

    Split render command

--- renderer.h
+++ renderer.h
@@ -9,35 +9,9 @@
 #include "shader.h"
 #include "bindable.h"
 #include "framebuffer.h"
+#include "command.h"
 
 static bool glfw_present;
-
-/// <summary>
-/// A singular command issued to the renderer during userland script execution.
-/// </summary>
-class RenderCommand {
-private:
-	glm::vec2 position;
-	glm::vec2 scale;
-	float rotation;
-	float layer;
-	Framebuffer* target = nullptr;
-	Bindable** bindables;
-	int bindables_length = 0;
-public:
-	RenderCommand();
-	static void export_type(sol::state &target);
-	static bool ord_layer(RenderCommand& a, RenderCommand& b);
-	RenderCommand& to(Framebuffer* buffer);
-	RenderCommand& with(Bindable* bindable);
-	RenderCommand& with_position(glm::vec2 position);
-	RenderCommand& with_position_xy(float x, float y);
-	RenderCommand& with_scale(glm::vec2 scale);
-	RenderCommand& with_scale_xy(float x, float y);
-	RenderCommand& with_rotation(float rotation);
-	RenderCommand& with_layer(float layer);
-	friend class Renderer;
-};
 
 /// <summary>
 /// An object that owns a system Window and allows drawing inside of it.
